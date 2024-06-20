605a534dd0494668af4cc34257c76ec322e8f171
pikalius <135007462+pikalius@users.noreply.github.com>
2024-06-20 21:10:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function calculateTotal() {
+  const com = parseFloat(document.querySelector('.js-com').value) || 0;
+  const cash = parseFloat(document.querySelector('.js-cash').value) || 0;
+  const pay = parseFloat(document.querySelector('.js-pay').value) || 0;
+  const transfer = parseFloat(document.querySelector('.js-transfer').value) || 0;
+  const keep = parseFloat(document.querySelector('.js-keep').value) || 0;
+
+  const total = cash + pay + transfer + keep;
+
+  document.getElementById('total').textContent = total;
+
+  const messageElement = document.getElementById('message');
+
+  if (total < com) {
+    messageElement.textContent = `เงินขาด ${com - total}`;
+  } else if (total === com) {
+    messageElement.textContent = `ดีใจด้วยจั๊ฟเงินไม่ขาด`;
+  } else if (total > com) {
+    messageElement.textContent = `เงินเกิน ${-(com - total)}`;
+  }
+}
